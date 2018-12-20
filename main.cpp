1bd35f7c7ba1a08acb949c4634ad4e50ceae91bf
slima <slima@btslivh.eu>
2018-12-20 17:52:37 +0100

    nouveau projet

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 20 décembre 2018, 17:49
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
